812d4184667b5cafd5497450a198ffe538b039f9
Samira <samirahekmati@gmail.com>
2025-04-08 09:56:04 +0100

    nodemon

--- node.js
+++ node.js
@@ -1,5 +1,7 @@
-const http = require ('http');
+const http = require ('http'); //native node module 
 
+//the http module has a createServer method that takes one call back F as an argument; the call back takes two args: 1.req 2.res
+//whenever http req is made createserver will run;
 const server = http.createServer((req, res)=>{
 
 // res =  our way of responding to the requester
@@ -9,14 +11,14 @@ const server = http.createServer((req, res)=>{
 //HTTP Status Codes:Three-digit codes that indicate the outcome of a request, such as "200 OK" or "404 Not Found"
 
 //header
-res.writeHead(200, {'content-body': 'text/html'})//writes header
+res.writeHead(200, {'content-Type': 'text/html'})//writes header
 
 //body
 res.write('<h1> Hello World form my sevrer! <h1>') //write body
 
 res.end() //let the browser know that we are ready toclose the connection
-
-
 })
 
-server.listen(3000)
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });//this server is listenening to the trafic on the port 3000
